e98e831190ca73ab548c8ccf958cfce80e7a149e
Nicole Birch <nicole.birch4@studytafensw.edu.au>
2024-10-15 10:50:05 +1100

    Added database connection

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express(); 
+
+const env = process.env;
+
+mongoose.connect(env.MONGO_URI)
+    .then(() => console.log('Database connected'))
+    .catch((err) => console.log(`Failed to connect ${err}`));
 
 const PORT = process.env.PORT || 8080;
 
