440a5fdb46531bf34ca8d2ee773acdf8c2cc374e
Nicole Birch <nicole.birch4@studytafensw.edu.au>
2024-10-15 11:06:17 +1100

    Server now only connects if database connects

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config(); 
 
+const app = express();
+
+const PORT = process.env.PORT || 8080;
 const env = process.env;
 
 mongoose.connect(env.MONGO_URI)
     .then(() => console.log('Database connected'))
+    .then(() => {
+        app.listen(PORT, () => { 
+            console.log(`Server listening on Port: ${PORT}`);
+        })
+    })
     .catch((err) => console.log(`Failed to connect ${err}`));
-
-const app = express();    
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => { 
-    console.log(`Server listening on Port: ${PORT}`);
-})
