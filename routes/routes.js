1e5f45966e80da2c1a54fb621d8bc4041240ea50
Nicole Birch <nicole.birch4@studytafensw.edu.au>
2024-10-18 14:05:22 +1100

    Product APIs functional

--- routes/routes.js
+++ routes/routes.js
@@ -2,64 +2,130 @@ import Product from '../models/Product.js';
 
 export default function Router(app){ 
     
-    // CRUD ops for Menu Items
+    // CRUD ops for Products
     app.post('/add-item', (req, res) => {
+        
         const newItem = new Product({
-            name: 'Chips',
-            price: 8.00,
-            course: 'Side'
+            name: add.name,
+            price: add.price,
+            course: add.course,
+            options: add.options, 
+            image: add.image
         });
-    
+
+//------> is this handling all possible errors? 
         newItem.save()
-            .then(result => {
-                res.send(result);
+            .then(() => {
+                res.status(200).send({
+                    success: true,
+                    msg: `${newItem} saved`
+                });
             })
             .catch(err => { 
-                res.status(500).send('Product not saved');
+                res.status(500).send({
+                    success: false,
+                    msg: err
+                });
             });
     });
     
-    app.get('/menu', (req, res) => { 
-        Product.find()
-            .then(result => { 
-                res.send(result);
-            })
-            .catch(err => {
-                res.status(404).send('Menu not found');
-            });
-    });
-    
-    app.get('/menu/:id', (req, res) => {
-        Product.findById(req.params.id)
-            .then(result => { 
-                res.send(result);
-            })
-            .catch(err => { 
-                res.status(404).send('Item not found');
-            });
-    });
 
+    app.get('/products', async (req, res) => { 
 
-    /*app.put('menu/:id', (req, res) => {
-        Product.findOneAndUpdate({ id: req.params.id })
+        try {
+            const allProducts = await Product.find({});
+
+            if (!allProducts) {
+                return res.status(400).send({
+                    success: false,
+                    msg: 'Product not found'
+                });
+            }
             
-    })*/
+            res.status(200).send({
+                success: true, 
+                msg: allProducts
+            });
+
+        } catch (err) { 
+            res.status(500).send({ status: false, msg: err });
+        };
+    });
+    
+
+    app.get('/products/:id', async (req, res) => {
+
+        try { 
+            const findProduct = await Product.findById(req.params.id);
+
+            if(!findProduct){
+                return res.status(400).send({
+                    success: false,
+                    msg: 'Product not found'
+                });
+            }
+
+            res.status(200).send({
+                status: true,
+                msg: findProduct
+            });
+
+        } catch (err) { 
+            res.status(500).send({ status: false, msg: err });
+        };
+    });
 
 
-    app.delete('/menu/:id', (req, res) => { 
-        Product.findOneAndDelete(req.params.id)
-            .then((result) => {
-                console.log(result)
-                res.status(200).send(`${result.name} deleted successfully`);
-            })
-            .catch(err => { 
-                res.status(404).send('Item not found');
-            })
+    app.put('/products/:id', async (req, res) => {
+
+        try { 
+            const updateProd = await Product.findByIdAndUpdate(req.params.id, body, { new: true })
+
+            if(!updateProd){
+                return res.status(400).send({
+                    success: false,
+                    msg: 'Product not found'
+                });
+            }
+
+            res.status(200).send({
+                success: true,
+                msg: `${updateProd.name} updated`
+        });
+
+        } catch (err) { 
+            res.status(500).send({ status: false, msg: err });
+        };
+    });
+
+
+    app.delete('/products/:id', async (req, res) => { 
+
+        try { 
+            const deleteProd = await Product.findByIdAndDelete(req.params.id);
+            
+            const deleteName = deleteProd.name;
+
+            if(!deleteProd){
+                return res.status(400).send({
+                    success: false,
+                    msg: 'Product not found'
+                });
+            }
+
+            res.status(200).send({
+                success: true,
+                msg: `${deleteName} deleted successfully`
+            });
+
+        } catch (err) { 
+            res.status(500).send({ status: false, msg: err });
+        }
     })
 
 
     // CRUD ops for Orders
-    app.post('order/:id', (req, res) => {
+    app.post('/new-order', (req, res) => {
 
     })
 
