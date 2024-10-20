6e33879c697234355f5818800269b473a9c60fd8
nicole <nicolebirchh@gmail.com>
2024-10-20 13:11:02 +1100

    Updated models to reflect schema design changes

--- models/Order.js
+++ models/Order.js
@@ -4,24 +4,18 @@ const {Schema, SchemaTypes, model} = mongoose;
 const orderSchema = new Schema({
     table: {
         type: SchemaTypes.ObjectId, 
-        ref: 'Table' },
+        ref: 'Table' ,
+        required: true },
     products: [{
         item: { 
             type: SchemaTypes.ObjectId, 
-            ref: 'Product' }, //I think that this is a zero-many, as the array is extensible but required is set to false
+            ref: 'Product' ,
+            required: true },
         quantity: { type: Number},
         isSent: { type: Boolean }
     }],
 },{ timestamps: true });
-/**
- * I think that as soon as a table is assigned then there is application logic for:
- *      1. TableNumber.products.push(this.products)
- *      2. POST this Order, and GET from the kitchen
- *      3. Once the order is marked "Sent" from the kitchen, destroy the object (memory management), as the important data
- *          is now stored in the Table so this object is irrelevant
- * 
- * Need to figure out logic for isSent bool
- */
+
 
 const Order = model('Order', orderSchema);
 export default Order;
