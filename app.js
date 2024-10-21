b42c260063d1007e021b5651ec931e21fafe3b6b
Ben Kindon <benkindon@gmail.com>
2024-10-21 18:27:21 +1100

    Altered schema to utilise Options for Products. Further comments in app.js

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import 'dotenv/config';
 import Router from './routes/routes.js';
-//import Seeder from './seeder/seeder.js';
+// import Seeder from './seeder/seeder.js';
 
+/**
+ * I wanted to make a new DB using the options schema, but I was having trouble getting products to reference options
+ * when being seeded as each time the options would have different IDs. My workaround is to manually first insert seeder/optionsNew.json 
+ * on MongoDB Compass, and then manually insert seeder/products.json. It aint pretty but it works.*/
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -15,9 +19,9 @@ const env = process.env;
 
 mongoose.connect(env.MONGO_URI)
     .then(() => console.log('Database connected'))
-    /*.then(() => {
-        Seeder();
-    })*/
+    // .then(() => {
+    //     Seeder();
+    // })
     .then(() => {
         app.listen(PORT, () => { 
             console.log(`Server listening on Port: ${PORT}`);
--- seeder/seeder.js
+++ seeder/seeder.js
@@ -1,7 +1,7 @@
-/*import items from './products.json' with { type: "json" };
-import tables  from './tables.json' with { type: "json" };
-import options from './options.json' with { type: "json" };
-*/
+import items from './products.json' with { type: "json" };
+// import tables  from './tables.json' with { type: "json" };
+// import options from './options.json' with { type: "json" };
+
 import Product from '../models/Product.js';
 import Table from '../models/Table.js';
 import Order from '../models/Order.js';
@@ -13,6 +13,17 @@ export default async function Seeder(){
         const ProductCheck = await Product.findOne(); 
         const TableCheck = await Table.findOne();
         const OptionCheck = await Option.findOne(); 
+
+        if (!OptionCheck){
+            for(const option of options){
+                const addOption = new Option({
+                    name: option.name,
+                    price: option.price
+                }, { timestamps: option.timestamps })
+                addOption.save(); 
+            }
+            console.log('DB Options seeded');
+        }
 
         if(!ProductCheck){
             for (const item of items){
@@ -27,33 +38,23 @@ export default async function Seeder(){
             }
             console.log('DB Product seeded');
         }
-        if (!TableCheck){
-            for (const table of tables){
-                const addTable = new Table({
-                    tableNo: table.tableNo,
-                    isOpen: table.isOpen,
-                    openedAt: table.openedAt,
-                    pax: table.pax,
-                    limit: table.limit,
-                    comment: table.comment,
-                    products: table.products,
-                    total: table.total
-                });
-                addTable.save();
-            }
-            console.log('DB Table seeded');
-        }
+        // if (!TableCheck){
+        //     for (const table of tables){
+        //         const addTable = new Table({
+        //             tableNo: table.tableNo,
+        //             isOpen: table.isOpen,
+        //             openedAt: table.openedAt,
+        //             pax: table.pax,
+        //             limit: table.limit,
+        //             comment: table.comment,
+        //             products: table.products,
+        //             total: table.total
+        //         });
+        //         addTable.save();
+        //     }
+        //     console.log('DB Table seeded');
+        // }
 
-        if (!OptionCheck){
-            for(const option of options){
-                const addOption = new Option({
-                    name: option.name,
-                    price: option.price
-                }, { timestamps: option.timestamps })
-                addOption.save(); 
-            }
-            console.log('DB Options seeded');
-        }
     } catch(err) { 
         console.log(`Error in seeding process: ${err}`);
     };    
