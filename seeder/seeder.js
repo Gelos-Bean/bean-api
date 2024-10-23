ccf29af5f2e897ba889c3325ba7f6598e00b8c39
Nicole Birch <nicole.birch4@studytafensw.edu.au>
2024-10-23 19:41:59 +1100

    Overhaul of seeder to make sure required collections are linked and seed correctly, data files also updated

--- seeder/seeder.js
+++ seeder/seeder.js
@@ -1,61 +1,99 @@
-import items from './products.json' with { type: "json" };
-// import tables  from './tables.json' with { type: "json" };
-// import options from './options.json' with { type: "json" };
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+import products from './products.json' with { type: "json" };
+import tables  from './tables.json' with { type: "json" };
+import options from './options.json' with { type: "json" };
 
 import Product from '../models/Product.js';
 import Table from '../models/Table.js';
-import Order from '../models/Order.js';
 import Option from '../models/Option.js';
 
+const env = process.env;
 
-export default async function Seeder(){  
+async function connect() {
     try { 
-        const ProductCheck = await Product.findOne(); 
-        const TableCheck = await Table.findOne();
-        const OptionCheck = await Option.findOne(); 
+        const db = await mongoose.connect(env.MONGO_URI);
 
-        if (!OptionCheck){
-            for(const option of options){
-                const addOption = new Option({
-                    name: option.name,
-                    price: option.price
-                }, { timestamps: option.timestamps })
-                addOption.save(); 
-            }
-            console.log('DB Options seeded');
+        if (!db) {
+            return console.log('Error connecting to database');
         }
+        console.log('Database connected');
 
-        if(!ProductCheck){
-            for (const item of items){
-                const addProd = new Product({
-                    name: item.name,
-                    price: item.price, 
-                    course: item.course,
-                    options: item.options,
-                    image: item.image,
-                }, {timestamps: item.timestamps});
-                addProd.save();
-            }
-            console.log('DB Product seeded');
-        }
-        if (!TableCheck){
-            for (const table of tables){
-                const addTable = new Table({
-                    tableNo: table.tableNo,
-                    isOpen: table.isOpen,
-                    openedAt: table.openedAt,
-                    pax: table.pax,
-                    limit: table.limit,
-                    comment: table.comment,
-                    products: table.products,
-                    total: table.total
+
+        const seed = await Seeder();
+        console.log('Data successfully seeded');
+
+    } catch (err) {
+        console.log('Error connecting/seeding database' + err);
+
+    } finally {
+        await mongoose.disconnect(); 
+        console.log('Database disconnected');
+    }
+}
+
+
+
+async function Seeder(){  
+    try { 
+
+        // reset database prior to seeding
+        // comment out if you do not want to delete data
+        await Product.deleteMany();
+        await Option.deleteMany();
+        await Table.deleteMany();
+
+        const optionData = await Option.insertMany(options);
+    
+        // take option ids and link the name to the product 
+        // items in the dummy data, then seed products db
+        const optionLookup = {};
+        optionData.forEach(option => {
+            optionLookup[option.name] = option._id; 
+        });
+
+        const addOptionId = products.map(product => ({
+            ...product,
+            options: product.options.map(o => optionLookup[o])
+        }));
+
+        await Product.insertMany(addOptionId);
+
+
+
+        // randomly assign products and quantities to table data
+        const seededProducts = await Product.find();
+        const productIds = seededProducts.map(product => product._id);
+        
+        const randomizeProductsForTable = () => {
+            const numberOfProducts = Math.floor(Math.random() * 5) + 1;
+            const selectedProducts = [];
+            
+            for (let i = 0; i < numberOfProducts; i++) {
+                const randomProduct = productIds[Math.floor(Math.random() * productIds.length)];
+                const randomQuantity = Math.floor(Math.random() * 5) + 1;
+                
+                selectedProducts.push({
+                    item: randomProduct,
+                    quantity: randomQuantity
                 });
-                addTable.save();
             }
-            console.log('DB Table seeded');
-        }
+
+            return selectedProducts;
+        };
+
+        const tablesWithProducts = tables.map(table => {
+            const products = randomizeProductsForTable();
+
+            return { ...table, products };
+        });
+
+        await Table.insertMany(tablesWithProducts);
 
     } catch(err) { 
         console.log(`Error in seeding process: ${err}`);
     };    
 };
+
+connect();
