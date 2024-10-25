02a695e3eb4bdad3607b9e19119232bfdfeefb71
Nicole Birch <nicole.birch4@studytafensw.edu.au>
2024-10-25 14:35:13 +1100

    Fixing merge issues

--- models/Table.js
+++ models/Table.js
@@ -2,15 +2,16 @@ import mongoose from "mongoose";
 const {Schema, model} = mongoose;
 
 const tableSchema = new Schema({
-    tableNo: { type: Number, required: true,  unique: true}, //Must also handle unique field validation in routes or frontend
+    tableNo: { type: Number, required: true,  unique: true}, //Must also handle unique field validation in application logic
     openedAt: { 
         type: Date, 
-        default: Date.now 
+        default: Date.now
       },
     pax: {type: Number, required: true},
     limit: { type: Number, required: false },
     products: [{
-        item: { type: Schema.Types.ObjectId, ref: 'Product' }, 
+        item: { type: Schema.Types.ObjectId, ref: 'Product' },
+        selectedOptions: [{ type: SchemaTypes.ObjectId, ref: 'Option' }],
         quantity: { type: Number}
     }],
     total: { type: Number, required: true },
