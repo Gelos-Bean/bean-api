7e2a335db942e230280f22adf721a5a73cb627f0
Nicole Birch <nicole.birch4@studytafensw.edu.au>
2024-10-25 17:02:25 +1100

    Added selectedOptions item to product props

--- models/Table.js
+++ models/Table.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
-const {Schema, model} = mongoose;
+const {Schema, SchemaTypes, model} = mongoose;
 
 const tableSchema = new Schema({
-    tableNo: { type: Number, required: true,  unique: true}, //Must also handle unique field validation in routes or frontend
+    tableNo: { type: Number, required: true,  unique: true}, //Must also handle unique field validation in application logic
     openedAt: { 
         type: Date, 
-        default: Date.now 
+        default: Date.now
       },
     pax: {type: Number, required: true},
     limit: { type: Number, required: false },
     products: [{
-        item: { type: Schema.Types.ObjectId, ref: 'Product' }, 
+        item: { type: SchemaTypes.ObjectId, ref: 'Product' },
+        selectedOptions: [{ type: SchemaTypes.ObjectId, ref: 'Option' }],
         quantity: { type: Number}
     }],
     total: { type: Number, required: true },
