783c6cc2271bc8f35d9778da0f81f414b64f271b
Nicole Birch <nicole.birch4@studytafensw.edu.au>
2024-10-26 11:00:38 +1100

    Schematypes not defined

--- models/Table.js
+++ models/Table.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-const {Schema, model} = mongoose;
+const {Schema, SchemaTypes, model} = mongoose;
 
 const tableSchema = new Schema({
     tableNo: { type: Number, required: true,  unique: true }, //Must also handle unique field validation in routes or frontend
@@ -10,7 +10,7 @@ const tableSchema = new Schema({
     pax: {type: Number, required: true},
     limit: { type: Number },
     products: [{
-        item: { type: Schema.Types.ObjectId, ref: 'Product' }, 
+        item: { type: SchemaTypes.ObjectId, ref: 'Product' }, 
         selectedOptions: [{ type: SchemaTypes.ObjectId, ref: 'Option' }],
         quantity: { type: Number}
     }],
