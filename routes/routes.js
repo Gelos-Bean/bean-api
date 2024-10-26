b9260f647a8c74cbabd24fe2adf52b80ac565692
Nicole Birch <nicole.birch4@studytafensw.edu.au>
2024-10-26 13:56:35 +1100

    Split endpoints for readability

--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,10 @@ import productRoutes from "./products.js";
 import tableRoutes from "./tables.js";
 
 export default function Router(app){ 
+
     app.use(['/add-option', '/options'], optionRoutes);
     app.use(['/add-order', '/orders'], orderRoutes);
     app.use(['/add-product', '/products'], productRoutes);
     app.use(['/add-table', '/tables'], tableRoutes);
+    
 };
